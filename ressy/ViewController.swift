07350ac11eeb746b11c0ffc1296334aff004a29a
Valeh <i.valehjr@gmail.com>
2023-11-19 17:34:21 +0400

    Initial Commit

---
+++ ressy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ressy
+//
+//  Created by Valeh Ismayilov on 19.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
